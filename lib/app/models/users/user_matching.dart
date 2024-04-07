d79b450ddbda58b7d125d71fb256c483473e327b
nb150301 <anhnguyen150301@gmail.com>
2024-04-07 15:34:43 +0700

    update app color base

---
+++ lib/app/models/users/user_matching.dart
@@ -0,0 +1,16 @@
+import 'package:dafa/app/models/app_user.dart';
+
+class UserMatching {
+  AppUser? user;
+  double distance;
+  UserMatching({
+    required this.user,
+    required this.distance,
+  });
+}
+
+// This using for analyst and give recommend for matching
+// This using for analyst and give recommend for matching
+
+// Path: SE346_Mobile_Programming/lib/app/models/users/user_matching.dart
+// Compare this snippet from SE346_Mobile_Programming/lib/app/core/variables/var_colors.dart:
